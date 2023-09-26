53e2ce87a73ae9627cc9b7d6131abdf672c679cd
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2023-09-26 07:02:46 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = "33abc" //let dectare variable Suppose we are play a game like Pokeymonn etc
+ // Suppose we need to find 'type' although we know that but somewhere we worked at back end.
+
+ //console.log(typeof score)
+ //console.log(typeof(score)) // 'Score' under Paranthesis is same as above one 'score'
+
+ //Suppose Our value came in 'String' and we want to do operation on Numbers
+ // So  we can declare new variable
+ // Note : 'let' is a Variable 
+
+ // We converted the number
+
+ let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+ //"33" => 33
+ //"33abc" => NaN
+ //true => 1 ; false =>0
+
+ let  isLoggedIn = 1
+
+ // We converted into boolean
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
+
+
+ // 1 => true; 0 => false
+ //"" => false
+ //"Adarsh" => true
+  
+
+ // We can also convert in 'String'
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+
+console.log(typeof stringNumber);
