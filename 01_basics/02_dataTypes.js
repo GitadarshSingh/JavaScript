7065e2ae7ba05501baaea07bb1d9ad5a4c2d2876
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2023-09-26 06:04:44 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,54 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nide js , not browser
+
+//console.log(3+3) console.log("Adarsh")//error will shown
+//Similarly
+//console.log(3+3); console.log("Adarsh") // Not error will came
+
+//Note : If output will came this means code is perfectly right because Readable is need to be necessary
+
+//Example :
+console.log(3
+    +
+    3) // code  readability should be high
+
+console.log("Adarsh")
+
+
+
+//-----------------------------------------------------------------------------------   
+
+
+//let name = "Adarsh" //String Datatype
+//let age = 22 // Numer datatype
+//let isLoggedIn = False // Boolean Datatype
+//let state = null // null is not zero ;
+
+
+//number => 2 to pwr 53
+//bigint - is used for big entry
+//string =>""
+//boolean => true/false
+
+//null => is a standalone value (representation of empty value means i want 
+//to know temp from server due to glitsch in server it says null(means value abhi aaya hee nhi hai))
+
+//undefined => whenever we declare variable but we can't assign value 
+
+//symbol => unique
+
+
+
+
+//object
+
+console.log(typeof undefined); // undefined datatype
+console.log(typeof null); // object datatype
+console.log(typeof "Adarsh")
+
+//-------------------------------------------------------------------------------------
+
+
+
+
