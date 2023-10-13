56ac19372ef6ed3dc8e11c53a650ea68ae0bc2f2
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2023-10-13 15:18:34 +0000

    switch.js

---
+++ 03_control_flow/switch.js
@@ -0,0 +1,36 @@
+// switch case
+
+// Syntax
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 5:
+        console.log("april");
+        break;
+    case 6:
+        console.log("June");
+        break;
+
+    default: console.log("Sorry");
+        break;
+} //  Output: march
